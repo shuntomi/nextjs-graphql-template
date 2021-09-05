815aae0f0033bd8087e4b3ebb9949e06bf5f6a6e
shuntomi <shuntomi.0730@gmail.com>
2021-09-05 23:12:24 +0900

    feat: dynamic importしてcsrにする

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,13 @@ import Head from 'next/head';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
+import dynamic from 'next/dynamic';
 import { ApolloClient } from '../utilities/apollo';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const SafeHydrate = dynamic(() => import('../components/SafeHydrate'), {
+    ssr: false,
+  });
   const client = ApolloClient.instance;
   return (
     <>
@@ -12,9 +16,11 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
         <title>Next Example</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <SafeHydrate>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </SafeHydrate>
     </>
   );
 };
