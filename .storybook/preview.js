4cceee1f9ed69beb95a336b5107bfc8e100d04f6
Shun Tomizawa <shuntomi.0730@gmail.com>
2021-09-05 23:02:22 +0900

    test: apolloをstorybookでモックできるように

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import * as nextImage from 'next/image';
+import { MockedProvider } from '@apollo/client/testing';
+
+// See: https://xenox.dev/next-image-with-storybookjs/
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -5,5 +14,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  apolloClient: {
+    MockedProvider,
   },
 };
