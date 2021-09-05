d71db57dea9fe7b700b27a602f920ee5acc230c9
shuntomi <shuntomi.0730@gmail.com>
2021-09-05 23:54:51 +0900

    chore: generate path

---
+++ src/utilities/$path.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// prettier-ignore
+export const pagesPath = {
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+// prettier-ignore
+export type PagesPath = typeof pagesPath
+
+// prettier-ignore
+export const staticPath = {
+  sample_png: '/sample.png'
+} as const
+
+// prettier-ignore
+export type StaticPath = typeof staticPath
