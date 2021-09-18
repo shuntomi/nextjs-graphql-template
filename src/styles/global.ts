a4138fd333a4e7e6dfc93e1c0bb1ad26dc7d31b5
shuntomi <shuntomi.0730@gmail.com>
2021-09-19 03:28:50 +0900

    feat: styleとmaterial uiの設定を整備

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+/**
+ * GlobalCSSを定義
+ */
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f8f8f8;
+  }
+`;
+export default GlobalStyle;
